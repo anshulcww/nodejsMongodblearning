f0fb94a811a014de259069135a2403e3fbc6f186
Anshul Verma <anshulverma96@gmail.com>
2018-01-09 23:05:21 +0530

    db_collection_server_setup_completed

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var http = require('http');
 var dt = require('./myfirstmodule');
-
-
+var fs = require('fs');
+var mongo = require('mongodb');
+//creating a web server
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently: " + dt.myDateTime());
-    res.end();
-}).listen(8080);
+    fs.readFile('index.html', function(err, data){
+    	res.writeHead(200, {'Content-Type': 'text/html'});// method is the status code, 200 means that all is OK
+	    res.write(data);
+	    res.end();	
+	   });
+	}).listen(8080);
+
+
